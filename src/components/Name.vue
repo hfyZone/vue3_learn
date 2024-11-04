b2c472672877521f3a39d2839a36bf9d9950b2cc
hfyZone <1178805691@qq.com>
2024-11-04 21:38:59 +0800

    computed

--- src/components/Name.vue
+++ src/components/Name.vue
@@ -4,31 +4,33 @@
       FirstName: <input v-model="firstName">
       LastName: <input v-model="lastName">
     </label>
-    <p>Your name is {{fullName}};</p>
-    <p>Your nick name is {{nickName}}</p>
+    <p>Your name is {{ fullName }};</p>
+    <p>Your nick name is {{ nickName }}</p>
     <button @click="changeNickName">修改别名</button>
   </div>
 </template>
 
 <script lang="ts" setup>
 import {computed, ref} from "vue";
+
 let firstName = ref('Default');
 let lastName = ref('Default');
 // 计算属性（只读）
-let fullName = computed(()=>{
-  return firstName.value + " " +lastName.value;
+let fullName = computed(() => {
+  return firstName.value + " " + lastName.value;
 });
 //计算属性（可修改）
 
 let nickName = computed({
-  get(){
+  get() {
     return lastName.value + firstName.value;
   },
-  set(val){
+  set(val) {
     firstName.value = "212";
     lastName.value = "3223"
   }
 });
+
 function changeNickName() {
   nickName.value = "2";
 }
